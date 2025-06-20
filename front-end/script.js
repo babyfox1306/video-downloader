208596b028b5397b363bffe3c50a088078e798bb
babyfox1306 <155075090+babyfox1306@users.noreply.github.com>
2025-06-20 11:52:53 +0700

    feat: Improve Lighthouse score with major optimizations

--- front-end/script.js
+++ front-end/script.js
@@ -1,3 +1,14 @@
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').then(registration => {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, err => {
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     // Lấy các phần tử cần thiết từ DOM
     const downloadButton = document.getElementById('download-btn');
